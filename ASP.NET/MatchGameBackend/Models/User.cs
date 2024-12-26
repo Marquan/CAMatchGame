2a38d560b379134db7fa05e1ffc9f58ef34d5df5
MAzrilHello <mazrilhello@gmail.com>
2024-12-26 17:38:28 +0800

    Added asp .net backend project into same repo

---
+++ ASP.NET/MatchGameBackend/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MatchGameBackend.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Password { get; set; }
+
+        [Required]
+        public bool IsPaidUser { get; set; }
+
+        //New field to store elapsed time for game fragment
+
+        public int GameTime { get; set; }
+    }
+}
