aa47a6fd690c6ffed95ed770b84a09339d036a3f
MAzrilHello <mazrilhello@gmail.com>
2025-01-03 19:16:12 +0800

    Changed asp .net

---
+++ ASP.NET/MatchGameBackend/Models/Game.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MatchGameBackend.Models
+{
+    public class Game
+    {
+        public Game()
+        {
+            GameId = Guid.NewGuid().ToString();
+        }
+        [Key]
+        public string GameId { get; set; }
+
+        [Required]
+        public int Duration { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+
+    }
+
+
+
+
+
+}
--- ASP.NET/MatchGameBackend/obj/Debug/net8.0/MatchGameBackend.AssemblyInfo.cs
+++ ASP.NET/MatchGameBackend/obj/Debug/net8.0/MatchGameBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatchGameBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40587ef6d4b2ff0b9c160239f47b526775a842f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+002114af54dc2208d1ad1fbbce377d042f906fb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatchGameBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatchGameBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
