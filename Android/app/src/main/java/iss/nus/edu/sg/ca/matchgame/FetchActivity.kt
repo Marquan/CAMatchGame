e8e549e98286a72db88f61fed2d49beb725b1f9a
Marquan <Marquan@users.noreply.github.com>
2025-01-06 11:13:54 +0800

    Revert "Merge Main to Marquan"

--- Android/app/src/main/java/iss/nus/edu/sg/ca/matchgame/FetchActivity.kt
+++ Android/app/src/main/java/iss/nus/edu/sg/ca/matchgame/FetchActivity.kt
@@ -167,13 +167,9 @@ class FetchActivity : AppCompatActivity() {
                 //reset the progress bar value to 0
                 progressBar.progress = 0
 
-                //reset images and disable button before finish downloading
+                //make sure imageButton is disabled first
                 runOnUiThread{
-                    val tempImg = MutableList(20) {
-                        BitmapFactory.decodeResource(resources, R.drawable.card_placeholder)
-                    }
                     adapter.updateEnableButtons(false)
-                    adapter.updateImages(tempImg)
                 }
 
                 //if service is available
